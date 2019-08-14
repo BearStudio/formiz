4426903921a4d5cafe6d131409f9498aaed2d338
Ivan Dalmet <ivan@dalmet.fr>
2019-08-14 17:41:30 +0200

    docs: Add main menu entry for demos

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,14 +1,9 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 module.exports = {
   docs: {
     Introduction: [
       'getting-started',
+    ],
+    Demos: [
       'demos/basic-demo',
     ],
     Core: [
