01d049ee43c2524ec156010f1daf9b4e45e69c30
Ivan Dalmet <ivan@dalmet.fr>
2019-08-25 15:00:58 +0200

    docs: Optimize tailwind for production

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,5 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './docs/**/*.js',
+    './docs/**/*.mdx',
+    './src/**/*.js',
+    './src/**/tailwind.css',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
@@ -8,5 +22,8 @@ module.exports = {
     require('cssnano')({
       preset: 'default',
     }),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : [],
   ],
 };
