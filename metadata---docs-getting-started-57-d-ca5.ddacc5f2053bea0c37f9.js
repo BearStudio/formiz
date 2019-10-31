4132c01509f11c734fa719e852411ee6e07d2aad
Ivan Dalmet <ivan@saagie.com>
2019-10-31 19:30:07 +0100

    Deploy website version based on f5ba88d1f0b1db364bc76273e2f207fce41b45f7

---
+++ metadata---docs-getting-started-57-d-ca5.ddacc5f2053bea0c37f9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{159:function(t){t.exports=JSON.parse('{"id":"getting-started","title":"Getting Started","description":"import { Alpha } from \'../src/messages\'","source":"@site/docs/getting-started.mdx","permalink":"/docs/getting-started","sidebar":"docs","next":{"title":"Wizard Form","permalink":"/docs/demos/wizard"}}')}}]);
