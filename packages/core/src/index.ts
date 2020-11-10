08c2f0a4e7da0b10714baa5b6b2e178bbeb73639
Ivan Dalmet <ivan@dalmet.fr>
2020-11-10 17:33:15 +0100

    fix(core): Field type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,14 +4,11 @@ export { useField } from './useField';
 export { useForm } from './useForm';
 export {
   UseFieldProps as FieldProps,
-  ExposedField as Field,
+  UseFieldValues as Field,
   fieldPropTypes,
   fieldDefaultProps,
 } from './types/field.types';
-export {
-  FormizStepProps as StepProps,
-  Step,
-} from './types/step.types';
+export { FormizStepProps as StepProps, Step } from './types/step.types';
 export {
   FormizProps as FormProps,
   UseFormValues as Form,
