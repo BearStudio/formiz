c4741b42b5cdce0dba2fb9ab2d9b1bb598cdc7f2
Ivan Dalmet <ivan@dalmet.fr>
2020-11-10 17:11:28 +0100

    feat(core): Expose Typescript types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,17 @@ export { Formiz } from './Formiz';
 export { FormizStep } from './FormizStep';
 export { useField } from './useField';
 export { useForm } from './useForm';
-export { fieldPropTypes, fieldDefaultProps } from './types/field.types';
+export {
+  UseFieldProps as FieldProps,
+  ExposedField as Field,
+  fieldPropTypes,
+  fieldDefaultProps,
+} from './types/field.types';
+export {
+  FormizStepProps as StepProps,
+  Step,
+} from './types/step.types';
+export {
+  FormizProps as FormProps,
+  UseFormValues as Form,
+} from './types/form.types';
