6afc8ebfcdf39855a65d2b06a8c045ec937ca952
Ivan Dalmet <ivan@dalmet.fr>
2021-09-23 10:26:04 +0200

    test(examples): Add cypress auto form tests

--- examples/cypress/integration/auto-form.spec.js
+++ examples/cypress/integration/auto-form.spec.js
@@ -1,5 +1,49 @@
 describe('AutoForm', () => {
-  it('Should do something', () => {
+  beforeEach(() => {
     cy.visit('/');
+    cy.pageTitleIs('Auto form');
+  });
+
+  it('Simple Case', () => {
+    cy.field('name').type('John');
+    cy.field('email').type('john@company.com');
+    cy.wait(1000);
+    cy.formSubmit();
+    cy.isFormSuccess();
+  });
+
+  it('Error Email', () => {
+    cy.field('email').type('john@company');
+    cy.formSubmit();
+    cy.field('email').hasError('Not a valid email');
+  });
+
+  it('Show errors', () => {
+    cy.formSubmit();
+    cy.field('name').hasError('Required');
+    cy.field('email').hasError('Required');
+  });
+
+  it('Set fields errors', () => {
+    cy.field('name').type('John');
+    cy.field('email').type('john@company.com');
+
+    cy.wait(1000);
+    cy.formSubmit();
+
+    cy.field('name').hasError('You can display an error after an API call');
+
+    cy.get('button[type="submit"][disabled]');
+  });
+
+  it('Set fields values', () => {
+    cy.field('name').type('John');
+    cy.field('company').type('Formiz');
+    cy.get('button').contains('Fill with').click();
+
+    cy.wait(1000);
+    cy.field('name').hasValue('John');
+    cy.field('email').hasValue('john@company.com');
+    cy.field('company').hasValue('Formiz');
   });
 });
--- examples/cypress/support/commands.js
+++ examples/cypress/support/commands.js
@@ -25,7 +25,49 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 /**
- * Custom command to select DOM element by data-test attribute.
- * @example cy.dateTest('greeting')
+ * Custom command to select a field element.
+ * @example cy.field('name')
  */
-Cypress.Commands.add('dataTest', (value) => cy.get(`[data-test=${value}]`));
+Cypress.Commands.add('field', (name) => cy.get(`[name="${name}"]`));
+
+/**
+ * Custom command to see if a field has an value
+ * @example cy.field('name').hasValue('John')
+ */
+Cypress.Commands.add(
+  'hasValue',
+  {
+    prevSubject: 'element',
+  },
+  (subject, value) => cy.get('input', { withinSubject: subject }).should('have.value', value),
+);
+
+/**
+ * Custom command to see if a field has an error
+ * @example cy.field('name').hasError('Error message')
+ */
+Cypress.Commands.add(
+  'hasError',
+  {
+    prevSubject: 'element',
+  },
+  (subject, error) => cy.wrap(subject).get('.chakra-form__error-message').should('contain', error),
+);
+
+/**
+ * Custom command to send the form.
+ * @example cy.submitForm()
+ */
+Cypress.Commands.add('formSubmit', () => cy.get('[type="submit"]').click());
+
+/**
+ * Custom command to check if the form sent successfully.
+ * @example cy.isFormSuccess()
+ */
+Cypress.Commands.add('isFormSuccess', () => cy.get('.chakra-toast').should('contain', 'Values submitted'));
+
+/**
+ * Custom command to check if the form sent successfully.
+ * @example cy.pageTitleIs('Auto form')
+ */
+Cypress.Commands.add('pageTitleIs', (title) => cy.get('[data-test="header"]').should('contain', title));
