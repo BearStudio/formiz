bd938c0079e86c9989a70f7433024bcecca9daca
Ivan Dalmet <ivan@dalmet.fr>
2021-09-23 13:41:51 +0200

    test(examples): Add cypress repeater tests

---
+++ examples/cypress/integration/wizard.test.js
@@ -0,0 +1,44 @@
+describe('Wizard', () => {
+  beforeEach(() => {
+    cy.visit('/wizard');
+    cy.pageTitleIs('Wizard');
+  });
+
+  it('Simple Case', () => {
+    cy.field('name').fill('John');
+    cy.formNextStep();
+
+    cy.field('email').fill('john@company.com');
+    cy.formNextStep();
+
+    cy.formSubmit();
+    cy.isFormSuccess();
+  });
+
+  it('Error on each step', () => {
+    cy.formNextStep();
+    cy.field('name').hasError('Required');
+    cy.field('name').fill('John');
+    cy.formNextStep();
+
+    cy.formNextStep();
+    cy.field('name').hasError('Required');
+    cy.field('email').fill('john@company.com');
+    cy.formNextStep();
+
+    cy.formSubmit();
+    cy.isFormSuccess();
+  });
+
+  it('Set field error', () => {
+    cy.field('name').fill('John');
+    cy.formNextStep();
+
+    cy.field('email').fill('john@company.com');
+    cy.formNextStep();
+
+    cy.formSubmit();
+    cy.field('email').hasError('You can display an error after an API call');
+    cy.get('button[type="submit"]').should('be.disabled');
+  });
+});
