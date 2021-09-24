e8964823bb792c5e98b156d8280e91a8811b99c5
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2021-09-24 11:02:35 +0100

    Cypress (#92)
    * test(examples): Setup Cypress
    
    * test(examples): Add cypress auto form tests
    
    * test(examples): Add cypress dynamic steps tests
    
    * test(examples): Add cypress repeater tests
    
    * test(examples): Add cypress repeater tests
    
    * test(examples): Remove Testcafe
    
    * test(examples): Migrate cypress tests to Typescript

---
+++ examples/cypress/support/commands.ts
@@ -0,0 +1,71 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('field', (name) => cy.get(`[name="${name}"]`));
+
+Cypress.Commands.add(
+  'fill',
+  {
+    prevSubject: 'element',
+  },
+  (subject, value) => cy
+    .get('input', { withinSubject: subject })
+    .clear()
+    .type(value),
+);
+
+Cypress.Commands.add(
+  'hasValue',
+  {
+    prevSubject: 'element',
+  },
+  (subject, value) => cy.get('input', { withinSubject: subject }).should('have.value', value),
+);
+
+Cypress.Commands.add(
+  'hasError',
+  {
+    prevSubject: 'element',
+  },
+  (subject, error) => cy
+    .wrap(subject)
+    .get('.chakra-form__error-message')
+    .should('contain', error),
+);
+
+Cypress.Commands.add('formNextStep', () => cy
+  .wait(0)
+  .get('[type="submit"]')
+  .click());
+
+Cypress.Commands.add('formSubmit', () => cy
+  .wait(0)
+  .get('[type="submit"]')
+  .click());
+
+Cypress.Commands.add('isFormSuccess', () => cy.get('.chakra-toast').should('contain', 'Values submitted'));
+
+Cypress.Commands.add('pageTitleIs', (title) => cy.get('[data-test="header"]').should('contain', title));
