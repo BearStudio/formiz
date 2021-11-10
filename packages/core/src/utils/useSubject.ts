191b6fcaec4f1aa53f11ebf6769bd74bae380a06
Ivan Dalmet <ivan@dalmet.fr>
2021-11-10 12:11:42 +0100

    fix(core): Remove throttle only for useSubject

--- packages/core/src/utils/useSubject.ts
+++ packages/core/src/utils/useSubject.ts
@@ -1,24 +1,36 @@
 import React, { useRef } from 'react';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
 
-export const useSubject = (valueRef?: React.RefObject<unknown>) => {
+export const useSubject = (valueRef?: React.RefObject<any>) => {
   const subjectRef = useRef(new Subject());
-  const push = (value?: unknown) => {
+  const push = (value?: any) => {
     subjectRef.current.next(value ?? valueRef?.current);
   };
 
-  const subscription = subjectRef.current.pipe();
+  const subscription = subjectRef.current;
 
   return { push, subscription };
 };
 
-export const useBehaviorSubject = (valueRef: React.RefObject<unknown>) => {
+export const useBehaviorSubject = (
+  valueRef: React.RefObject<any>,
+  throttle = 100,
+) => {
   const subjectRef = useRef(new BehaviorSubject(valueRef.current));
-  const push = (value?: unknown) => {
+  const push = (value?: any) => {
     subjectRef.current.next(value ?? valueRef.current);
   };
 
-  const subscription = subjectRef.current.pipe();
+  const subscription = !throttle
+    ? subjectRef.current
+    : subjectRef.current.pipe(
+        throttleTime(throttle, undefined, {
+          leading: true,
+          trailing: true,
+        }),
+      );
+
 
   return { push, subscription };
 };
