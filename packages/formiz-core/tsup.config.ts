eb31db511b8f20facfc2fb6d3862f4cf0d4867f4
Ivan Dalmet <ivan@dalmet.fr>
2023-01-31 13:35:10 +0100

    fix: Remove env dependency in bundle

--- packages/formiz-core/tsup.config.ts
+++ packages/formiz-core/tsup.config.ts
@@ -1,14 +1,12 @@
 import type { Options } from "tsup";
 
-const env = process.env.NODE_ENV;
-
 export const tsup: Options = {
   splitting: true,
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   format: ["cjs", "esm"], // generate cjs and esm files
-  minify: env === "production",
-  bundle: env === "production",
+  bundle: true,
+  skipNodeModulesBundle: false,
   entryPoints: ["src/index.ts"],
   target: "es2020",
   external: ["react"],
