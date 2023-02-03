72c75b1ee58458352328f16b7b988d95e18f8f7e
Gregoire-Bearstudio <80386314+Gregoire-Bearstudio@users.noreply.github.com>
2023-02-03 17:46:10 +0100

    Add Documentation (#142)
    * useField completed check
    
    * Add to useField documentation new elements
    
    * edit useFormsFields documentation
    
    * Change id description on <Formiz> page
    
    * useForm and useFormContext edit
    
    * Add FormizStep documentation
    
    * fix ts config
    
    * edit functions type in useFields and fix resetKey type
    
    * Fix Wording
    
    * add code editor in pages
    
    * add useFormContextExample
    
    * Add rawData to useField doc
    
    * Fix: Spelling mistakes
    
    * fix: Improve <Formiz> documentation
    
    * fix: Remove try it sections
    
    ---------
    
    Co-authored-by: Dylan Flandrin <dylan.flandrin@bearstudio.fr>
    Co-authored-by: Ivan Dalmet <ivan@dalmet.fr>

--- apps/docs/src/templates/useFormFields.ts
+++ apps/docs/src/templates/useFormFields.ts
@@ -1,20 +1,32 @@
-export const useFormFiles = [
+export const useFormFields = [
   {
     name: "index.js",
     code: `
-import { Formiz, useForm } from '@formiz/core';
+import { Formiz, useForm, useFormFields } from '@formiz/core';
 import { Field } from './field.ts';
 
 export default function Example() {
   const form = useForm();
 
+  const selectedFields = useFormFields({
+    connect: form,
+    fields: ["firstName"],
+    selector: (field) => ({isPristine: field.isPristine, isValid: field.isValid})
+  });
+
   return (
     <Formiz autoForm connect={form}>
       <Field
-        name="my-field"
+        name="firstName"
         required="I'm required"
+        placeholder
       />
-      <pre>{JSON.stringify(form ?? {}, 0 , 2)}</pre>
+      <br />
+      <Field
+      name="lastName"
+      required="I'm required"
+    />
+      <pre>{JSON.stringify(selectedFields ?? {}, 0 , 2)}</pre>
       <button type="submit">Valider</button>
     </Formiz>
   );
