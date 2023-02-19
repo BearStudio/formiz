1349efe036ef84d2e3134a26718a973231aad2f5
Hugo Pérard <hugo.perard@bearstudio.fr>
2023-02-19 10:41:27 +0100

    feat: theme config SEO

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 import logo from "./public/logo.svg";
@@ -18,6 +19,23 @@ const config: DocsThemeConfig = {
   primaryHue: { light: 100, dark: 115 },
   sidebar: {
     toggleButton: true,
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Formiz" />
+      <meta
+        property="og:description"
+        content="React forms with ease! Composable, headless & with built-in multi steps"
+      />
+    </>
+  ),
+  useNextSeoProps: () => {
+    const { route } = useRouter();
+    const titleTemplate = route === "/" ? "Formiz" : "%s - Formiz";
+    return {
+      titleTemplate,
+    };
   },
 };
 
