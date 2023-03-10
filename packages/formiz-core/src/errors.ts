dcb4d352b89a976dd047c9f188ee8aac21954684
Hugo Pérard <48803115+HugoPerard@users.noreply.github.com>
2023-03-10 13:40:46 +0100

    feat: repeater add data type (#173)

--- packages/formiz-core/src/errors.ts
+++ packages/formiz-core/src/errors.ts
@@ -6,3 +6,5 @@ export const ERROR_USE_FIELD_MISSING_CONTEXT =
   "A Formiz field (component using `useField` hook) always needs to be a children of a `<Formiz>` component.";
 export const ERROR_USE_FORM_CONTEXT_MISSING_CONTEXT =
   "`useFormContext` hook always needs to be a children of a `<Formiz>` component.";
+export const ERROR_USE_REPEATER_INITIAL_VALUES_NOT_ARRAY =
+  "Repeater initial values needs to be an array.";
